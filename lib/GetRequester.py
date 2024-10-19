f762c80395bec2d4164075adf77dc6e60d8c02ea
Sundeep <sundeep.kaler@gmail.com>
2024-10-19 03:17:17 -0400

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,7 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        return requests.get(self.url).content
 
     def load_json(self):
-        pass
+        return requests.get(self.url).json()
